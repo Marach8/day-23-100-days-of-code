3d3e7a82f03392970a5267a0d7129463d91ca2a5
Marach8 <nnannamarach4@gmail.com>
2023-09-12 11:49:27 +0000

    Learned how to reuse code using subroutines. Built a login system.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def askUser():
       print('\033[32mWelcome to Replit!\033[0m')
       break
     else:
-      print('\033[31mWhoops! I do not recognize that username or password\033[0m')
+      print('\033[31mWhoops! I do not recognize that username or password.\033[0m')
       print()
       print('Try again!')
       print()
